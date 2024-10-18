0ad0e018c46b198b4016bd999fc86fa79a60dd95
aryam003 <aryamarya537@gmai.com>
2024-10-18 13:37:49 +0530

    django-work

---
+++ project/app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def fun1(request):
+    return HttpResponse("welcome")
+
+def fun2(request):
+    return HttpResponse("hello")
+
+def fun3(req,a,b):
+    return HttpResponse(a)
+
+def fun4(req,a):
+    print(type(a))
+    return HttpResponse(a)
+
+def fun5(req,a,b,c):
+    if a>b and a>c:
+        return HttpResponse(a)
+    elif b>a and b>c:
+        return HttpResponse(b)
+    else:
+        return HttpResponse(c)
+
+def index_page(req):
+    name="Anu"
+    age=20
+    place="kannur"
+    return render(req,'index.html',{'name':name,'age':age,'place':place})
+
+def demo(req):
+    # l=[1,2,3,4,5]            #list
+    # l={'name':'anu','age':20}  
+    l=[{'name':'anu','age':20},{'name':'A','age':21},{'name':'B','age':20}]
+    d={'name':'anu','age':20}
+    return render(req,"demo.html",{"data":l,'data1':d})
+
+def snd(req):
+    return render(req,'second.html')
+
